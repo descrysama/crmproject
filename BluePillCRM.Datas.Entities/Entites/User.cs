63ab7ebc2c5b53ebf2ec746e65bc3363044691ab
descrysama <erwing2002@gmail.com>
2023-06-01 00:53:53 +0200

    Ajoutez des fichiers projet.

---
+++ BluePillCRM.Datas.Entities/Entites/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BluePillCRM.Datas.Entities;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public int Role { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
